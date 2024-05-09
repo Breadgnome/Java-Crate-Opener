76bf785adc35ccae949ce292fc0bb68b670ca764
Gavin <114312646+Breadgnome@users.noreply.github.com>
2024-05-09 18:37:27 +0000

    New classes  Changes to be committed: 	new file:   Help.class 	new file:   Main.java 	new file:   Welcome.class

---
+++ Main.java
@@ -0,0 +1,10 @@
+import Welcome.wU
+
+public class Main{
+public static void main (String[] args) throws InterruptedException // I just learned the throws function, this allows me to have a pause in time between printing, this gives it a nicer feel
+    {
+        Welcome wU = new Welcome();
+        wU.welcomeUser;
+        
+    }
+}
